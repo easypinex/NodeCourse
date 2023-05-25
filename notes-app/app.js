a01ae1a309fa29cf64535ce6a0366e09316c3117
Andy <easypinex@gmail.com>
2023-05-25 16:32:46 +0800

    4-16 course - Install and Using "yargs"

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,9 +1,11 @@
 const validator = require('validator')
 const chalk = require('chalk')
-
-const command = process.argv[2]
-if (command === 'add') {
-    console.log('Adding  note!')
-} else if (command === 'remove') {
-    console.log('Removing note!')
-}
+const yargs = require('yargs')
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function() {
+        console.log('Add a note.')
+    }
+});
+console.log(yargs.argv)
