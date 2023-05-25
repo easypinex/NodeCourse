43e14d87691c047b6276fc573d83c1080c27a6d1
Andy <easypinex@gmail.com>
2023-05-25 16:36:32 +0800

    4-16 challenge - using "yarg" to create command.

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,11 +1,40 @@
 const validator = require('validator')
 const chalk = require('chalk')
 const yargs = require('yargs')
+
+// Create Add Command
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
     handler: function() {
         console.log('Add a note.')
+    }
+});
+
+// Create Remove Command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function() {
+        console.log('Remove a note.')
+    }
+});
+
+// Create List Command
+yargs.command({
+    command: 'list',
+    describe: 'List notes',
+    handler: function() {
+        console.log('List notes.')
+    }
+});
+
+// Create Read Command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function() {
+        console.log('Read a note.')
     }
 });
 console.log(yargs.argv)
