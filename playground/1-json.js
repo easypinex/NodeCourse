6225eca3087436bd2a2e30f451d8e320f26388fe
Andy <easypinex@gmail.com>
2023-05-26 10:53:18 +0800

    4-18 challenge - read and modify json file

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,19 +1,20 @@
 const fs = require('fs')
 
-// Define dictionary 
-const book = {
-    title: 'Ego is the Enemy',
-    author: 'Ryan Holiday'
-}
-// Transform to json string
-const bookJson = JSON.stringify(book)
+// // Define dictionary 
+// const book = {
+//     title: 'Ego is the Enemy',
+//     author: 'Ryan Holiday'
+// }
+// // Transform to json string
+// const bookJson = JSON.stringify(book)
 // Write to file
-fs.writeFileSync('1-json.json', bookJson)
+// fs.writeFileSync('1-json.json', bookJson)
 // Read from file
 const dataBuffer = fs.readFileSync('1-json.json')
 // Transform to json string
 const dataJson = dataBuffer.toString()
 // Transform to json
 const data = JSON.parse(dataJson);
-// Get property form json
-console.log(data.title)
+data.name = 'Perry'
+data.age = 28
+fs.writeFileSync('1-json.json', JSON.stringify(data))
