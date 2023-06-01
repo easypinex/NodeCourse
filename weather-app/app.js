e8508822df6dc76c7e3bb10f068de644acd0b730
Andy <easypinex@gmail.com>
2023-06-01 14:39:39 +0800

    6-32 - challenge - get http body column from json.

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,5 +2,5 @@ const request = require('request')
 const url = 'http://api.weatherstack.com/current?access_key=e55cb8bafc3a5c57cae7888dfbb7d0d4&query=fetch:ip'
 request({'url': url}, (error, response) => {
     const data = JSON.parse(response.body)
-    console.log(data.current)
+    console.log(`It is currently ${data.current.temperature} degress out. It feels like ${data.current.feelslike} degress out.`)
 })
