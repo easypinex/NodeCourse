bbbc43b5b3ee74b9d92d63e14e19932ae781db23
Andy <easypinex@gmail.com>
2023-06-01 14:47:28 +0800

    6-32 - course - get json body arrays's column from http request.

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,6 +1,6 @@
 const request = require('request')
-const url = 'http://api.weatherstack.com/current?access_key=e55cb8bafc3a5c57cae7888dfbb7d0d4&query=fetch:ip'
+const url = 'http://api.weatherstack.com/current?access_key=e55cb8bafc3a5c57cae7888dfbb7d0d4&query=fetch:ip&units=f'
 request({'url': url}, (error, response) => {
     const data = JSON.parse(response.body)
-    console.log(`It is currently ${data.current.temperature} degress out. It feels like ${data.current.feelslike} degress out.`)
+    console.log(`${data.current.weather_descriptions[0]}. It is currently ${data.current.temperature} degress out. It feels like ${data.current.feelslike} degress out.`)
 })
