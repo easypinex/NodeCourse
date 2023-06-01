d1d2c06f73291d532b79c491e663bfc10e3a9b75
Andy <easypinex@gmail.com>
2023-06-01 13:44:41 +0800

    6-31 - course - http request for weather

---
+++ weather-app/app.js
@@ -0,0 +1,6 @@
+const request = require('request')
+const url = 'http://api.weatherstack.com/current?access_key=e55cb8bafc3a5c57cae7888dfbb7d0d4&query=fetch:ip'
+request({'url': url}, (error, response) => {
+    const data = JSON.parse(response.body)
+    console.log(data.current)
+})
