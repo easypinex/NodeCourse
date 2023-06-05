43d071a26e85da4456298b7f474351c2cc185733
Andy <easypinex@gmail.com>
2023-06-05 14:50:32 +0800

    6-37 - challenge - implement forecast util

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,20 +1,20 @@
 
+const forecast = require('./utils/forecast');
 const geocode = require('./utils/geocode')
-// const weather_url = 'http://api.weatherstack.com/current?access_key=e55cb8bafc3a5c57cae7888dfbb7d0d4&query=fetch:ip&units=f'
-// request({'url': weather_url, json:true}, (error, response) => {
-//     if (error) {
-//         if (error.body && error.body.info) {
-//             console.log(error.body.info)
-//             return
-//         }
-//         console.log('Unalbe to connect to weather service!')
-//         return;
-//     }
-//     const data = response.body
-//     console.log(`${data.current.weather_descriptions[0]}. It is currently ${data.current.temperature} degress out. It feels like ${data.current.feelslike} degress out.`)
-// })
+
 
 geocode('新北市板橋區莒光路135號3樓', (error, data) => {
-    console.log(error);
+    if (error) {
+        console.log(error);
+        return
+    }
     console.log(data)
+})
+
+forecast(121.470305, 25.024852, (error, data) => {
+    if (error) {
+        console.log(error);
+        return
+    }
+    console.log(`${data.current.weather_descriptions[0]}. It is currently ${data.current.temperature} degress out. It feels like ${data.current.feelslike} degress out.`)
 })
