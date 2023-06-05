2f2cac884cfc783bf58cef3e92990ccd0838cb0e
Andy <easypinex@gmail.com>
2023-06-05 10:54:07 +0800

    6-36 - course - use callback make geocode util.

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,4 +1,5 @@
-const request = require('request')
+
+const geocode = require('./utils/geocode')
 // const weather_url = 'http://api.weatherstack.com/current?access_key=e55cb8bafc3a5c57cae7888dfbb7d0d4&query=fetch:ip&units=f'
 // request({'url': weather_url, json:true}, (error, response) => {
 //     if (error) {
@@ -13,16 +14,7 @@ const request = require('request')
 //     console.log(`${data.current.weather_descriptions[0]}. It is currently ${data.current.temperature} degress out. It feels like ${data.current.feelslike} degress out.`)
 // })
 
-const geo_url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/12whatgg.json?access_token=pk.eyJ1IjoiZWFzeXBpbmV4IiwiYSI6ImNsaWNzN2lpdjBnNnYzaHF2bGNrdGhseTIifQ.ldFtXeV-Qm2vEq6kCG3kAw'
-request({url: geo_url, json:true}, (error, response) => {
-    if (error) {
-        console.log('Unalbe to connect to weather service!')
-        return
-    }
-   if (response.body.features.length == 0) {
-        console.log('Unable to find location. Try another search!')
-        return
-   }
-    const data = response.body
-    console.log(`Latitude: ${data.features[0].center[1]}, Longtitude: ${data.features[0].center[0]}`)
+geocode('新北市板橋區莒光路135號3樓', (error, data) => {
+    console.log(error);
+    console.log(data)
 })
