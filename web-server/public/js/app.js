998d6cc1878a44046ec0bb61a68070c03b94906f
Andy <easypinex@gmail.com>
2023-06-26 16:14:38 +0800

    8-58 - challenge - Creating a Search Form

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -6,8 +6,13 @@ fetch('https://puzzle.mead.io/puzzle').then((response) => {
     })
 })
 
-address = 'boston'
-fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=pk.eyJ1IjoiZWFzeXBpbmV4IiwiYSI6ImNsaWNzN2lpdjBnNnYzaHF2bGNrdGhseTIifQ.ldFtXeV-Qm2vEq6kCG3kAw`).then(
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const address = search.value
+    if (!address) return console.log('You must give search value')
+    fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=pk.eyJ1IjoiZWFzeXBpbmV4IiwiYSI6ImNsaWNzN2lpdjBnNnYzaHF2bGNrdGhseTIifQ.ldFtXeV-Qm2vEq6kCG3kAw`).then(
     (response) => {
         response.json().then(data => {
             if (data.error) return console.log(data.error)
@@ -28,11 +33,4 @@ fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(ad
         })
     }
 )
-
-const weatherForm = document.querySelector('form')
-const search = document.querySelector('input')
-weatherForm.addEventListener('submit', (e) => {
-    e.preventDefault()
-    const location = search.value
-    console.log(location)
 })
