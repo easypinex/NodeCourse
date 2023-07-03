68755b64b74cf40feac35e045304e481efa6bcbc
Andy <easypinex@gmail.com>
2023-07-03 16:47:10 +0800

    10-78 - course - Read Data with Query

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,4 +1,4 @@
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 // Connection URL
 const url = 'mongodb://localhost:27017';
 const client = new MongoClient(url);
@@ -11,44 +11,11 @@ async function main() {
     await client.connect();
     console.log('Connected successfully to server');
     const db = client.db(dbName);
-
-    //   await db.collection('users').insertOne({
-    //     name: 'Perry',
-    //     age: 29
-    //   });
-
-    // const result = await db.collection('users').insertMany([
-    //     {
-    //         name: 'Jen',
-    //         age: 23
-    //     },
-    //     {
-    //         name: 'Gunter',
-    //         age: 26
-    //     },
-    // ]);
-    // if (!result.acknowledged) {
-    //     console.error('insert error.')
-    // }
-
-    const result = await db.collection('tasks').insertMany([
-        {
-            description: 'Done of Section 10 course',
-            completed: false
-        },
-        {
-            description: '完成事中驗證',
-            completed: false
-        },
-        {
-            description: 'Jenkins 部署設置',
-            completed: true
-        },
-    ]);
-    if (!result.acknowledged) {
-        return console.error('ops! insert error.')
-    }
-    
+    const query = { name: 'Perry' };
+    const result = await db.collection('users').find(query).toArray();
+    const count = await db.collection('users').countDocuments(query);
+    console.log(result)
+    console.log(count)
     return 'done.';
 }
 
