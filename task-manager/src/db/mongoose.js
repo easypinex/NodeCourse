eaad948bb72d5dd60afc77b47ca3607246182991
Andy <easypinex@gmail.com>
2023-07-04 10:46:58 +0800

    11-84 - challenge - Using Mongoose Create Model and Data

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,13 +1,25 @@
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost:27017/task-manager-api')
-const User = mongoose.model('User', {
-    name: {
+// const User = mongoose.model('User', {
+//     name: {
+//         type: String
+//     },
+//     age: {
+//         type: Number
+//     }
+// })
+
+// const me = new User({ name: 'Perry', age: 'Mike' })
+// me.save().then(()=>console.log('Saved.', me))
+
+const Task = mongoose.model('Task', {
+    description: {
         type: String
     },
-    age: {
-        type: Number
+    completed: {
+        type: Boolean
     }
 })
 
-const me = new User({ name: 'Perry', age: 'Mike' })
-me.save().then(()=>console.log('Saved.', me))
+const task = new Task({ description: '提交OA單', completed: false })
+task.save().then(()=>console.log('saved, ', task))
