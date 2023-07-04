13ddc4ef3fe2847c651e8e1d85c4b1d18d6e2bce
Andy <easypinex@gmail.com>
2023-07-04 17:33:04 +0800

    11-90 - challenge - Rest API , model save to DB

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -2,16 +2,3 @@ const mongoose = require('mongoose')
 const validator = require('validator')
 
 mongoose.connect('mongodb://localhost:27017/task-manager-api')
-
-
-const Task = mongoose.model('Task', {
-    description: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    }
-})
--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 require('./db/mongoose')
 const User = require('./model/user')
-
+const Task = require('./model/task')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -11,7 +11,16 @@ app.use(express.json())
 app.post('/users', (req, res) => {
     const user = new User(req.body)
     user.save().then(()=>{
-        res.send(user)
+        res.status(201).send(user)
+    }).catch((err)=>{
+        res.status(400).send(err)
+    })
+})
+
+app.post('/tasks', (req, res) => {
+    const task = new Task(req.body)
+    task.save().then(()=>{
+        res.status(201).send(task)
     }).catch((err)=>{
         res.status(400).send(err)
     })
