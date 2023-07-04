9f875e17bc081569f8d817b75cc54833560d1a67
Andy <easypinex@gmail.com>
2023-07-04 14:30:32 +0800

    11-86 - challenge - Using Mongoose and validator to normalize model data

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -5,12 +5,12 @@ mongoose.connect('mongodb://localhost:27017/task-manager-api')
 const User = mongoose.model('User', {
     name: {
         type: String,
-        require: true,
+        required: true,
         trim: true
     },
     email: {
         type: String,
-        require: true,
+        required: true,
         trim: true,
         lowercase: true,
         validate(val) {
@@ -24,23 +24,37 @@ const User = mongoose.model('User', {
         validate(val) {
             if (val < 0) throw new Error('Age must be postive number')
         }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+        trim: true,
+        validate(val) {
+            if (val.toLowerCase().includes('password'))
+                throw new Error('Password doesn\'t contain "password"')
+        }
     }
 })
 
 const me = new User({ 
     name: 'Perry   ', 
-    email: 'perry@gmail.COM   ' 
+    email: 'perry@gmail.COM   ',
+    password: 'Phone!@GOOD'
 })
 me.save().then(()=>console.log('Saved.', me))
 
-// const Task = mongoose.model('Task', {
-//     description: {
-//         type: String
-//     },
-//     completed: {
-//         type: Boolean
-//     }
-// })
+const Task = mongoose.model('Task', {
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
+})
 
-// const task = new Task({ description: '提交OA單', completed: false })
-// task.save().then(()=>console.log('saved, ', task))
+const task = new Task({ description: '提交OA單', completed: false })
+task.save().then(()=>console.log('saved, ', task))
