6eb35122d1a2658dbbdb82428e56ba5a2b46131f
Andy <easypinex@gmail.com>
2023-07-04 14:10:44 +0800

    11-85 - course - Using Mongoose and validator to normalize model data

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,25 +1,46 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
+
 mongoose.connect('mongodb://localhost:27017/task-manager-api')
-// const User = mongoose.model('User', {
-//     name: {
-//         type: String
-//     },
-//     age: {
-//         type: Number
-//     }
-// })
-
-// const me = new User({ name: 'Perry', age: 'Mike' })
-// me.save().then(()=>console.log('Saved.', me))
-
-const Task = mongoose.model('Task', {
-    description: {
-        type: String
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        require: true,
+        trim: true
     },
-    completed: {
-        type: Boolean
+    email: {
+        type: String,
+        require: true,
+        trim: true,
+        lowercase: true,
+        validate(val) {
+            if (!validator.isEmail(val)) 
+                throw new Error('Email is invalid.')
+        }
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(val) {
+            if (val < 0) throw new Error('Age must be postive number')
+        }
     }
 })
 
-const task = new Task({ description: '提交OA單', completed: false })
-task.save().then(()=>console.log('saved, ', task))
+const me = new User({ 
+    name: 'Perry   ', 
+    email: 'perry@gmail.COM   ' 
+})
+me.save().then(()=>console.log('Saved.', me))
+
+// const Task = mongoose.model('Task', {
+//     description: {
+//         type: String
+//     },
+//     completed: {
+//         type: Boolean
+//     }
+// })
+
+// const task = new Task({ description: '提交OA單', completed: false })
+// task.save().then(()=>console.log('saved, ', task))
