8c0361a4b259103be4e04890610760278ce926f6
Andy <easypinex@gmail.com>
2023-07-06 11:03:49 +0800

    11-96 - course - Async.Await

--- task-manager/playground/promise-chaining.js
+++ task-manager/playground/promise-chaining.js
@@ -1,12 +1,24 @@
 require('../src/db/mongoose')
 const User = require('../src/models/user')
-User.findByIdAndUpdate('64a3ceb372d0aeb21c9f26bc', {
-    age: 1
-}).then(user => {
-    console.log(user)
-    return User.countDocuments({ age: 1 })
-}).then(result => {
-    console.log(result)
+// User.findByIdAndUpdate('64a3ceb372d0aeb21c9f26bc', {
+//     age: 1
+// }).then(user => {
+//     console.log(user)
+//     return User.countDocuments({ age: 1 })
+// }).then(result => {
+//     console.log(result)
+// }).catch(e => {
+//     console.log(e)
+// })
+
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, { age })
+    const count = await User.countDocuments({ age })
+    return count
+}
+
+updateAgeAndCount('64a3ceb372d0aeb21c9f26bc', 2).then(count => {
+    console.log(count)
 }).catch(e => {
     console.log(e)
 })
