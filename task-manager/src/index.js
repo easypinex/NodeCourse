f1112e3d6398c56ce31e7b89aeaa7ed079c0933b
Andy <easypinex@gmail.com>
2023-07-06 15:08:18 +0800

    11-101 - course - Separate File with Router.

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,68 +1,14 @@
 const express = require('express')
 require('./db/mongoose')
-const User = require('./models/user')
 const Task = require('./models/task')
 
 const app = express()
 const port = process.env.PORT || 3000
 
+const user_router = require('./routers/users')
+
 app.use(express.json())
-
-app.get('/users', async (req, res) => {
-    try {
-        const users = await User.find({});
-        res.status(200).send(users);
-    } catch (e) {
-        res.status(500).send(e)
-    }
-})
-
-app.get('/users/:id', async (req, res) => {
-    try {
-        const user = await User.findById(req.params.id);
-        if (!user) return res.status(404).send()
-        res.status(200).send(user);
-    } catch (e) {
-        res.status(400).send(e)
-    }
-})
-
-app.post('/users', async (req, res) => {
-    try {
-        const user = new User(req.body)
-        await user.save();
-        res.status(201).send(user)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-})
-
-app.patch('/users/:id', async (req, res) => {
-    try {
-        const updates = Object.keys(res.body)
-        const allow_updates = ['name', 'age', 'password', 'email']
-        const valid = updates.every((update) => allow_updates.includes(update))
-        if (!valid) return res.status(400).send({ error: 'Invalid updates!' })
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-            new: true,
-            runValidators: true
-        })
-        if (!user) return res.status(404).send()
-        res.send(user)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-})
-
-app.delete('/users/:id', async (req, res) => {
-    try {
-        const user = await User.findByIdAndDelete(req.params.id)
-        if (!user) return res.status(404).send()
-        res.send(user)
-    } catch (e) {
-        res.status(500).send(e)
-    }
-})
+app.use(user_router)
 
 app.get('/tasks', async (req, res) => {
     try {
