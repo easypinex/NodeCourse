5f73fc195c00a8035ad241556a377d91f670494a
Andy <easypinex@gmail.com>
2023-07-12 10:12:13 +0800

    13-118 - challenge - setup up timestamps in schema

--- task-manager/src/models/task.js
+++ task-manager/src/models/task.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 
-const Task = mongoose.model('Task', {
+const task_schema = new mongoose.Schema({
     description: {
         type: String,
         required: true,
@@ -16,6 +16,9 @@ const Task = mongoose.model('Task', {
         required: true,
         ref: 'User'
     }
+}, {
+    timestamps: true
 })
+const Task = mongoose.model('Task', task_schema)
 
 module.exports = Task
