c37088e30de7ee4cb216751b2f607c256cbb5e2a
Andy <easypinex@gmail.com>
2023-07-13 10:10:53 +0800

    14-123 - course - Using Multer Library to Support Upload File.

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -7,6 +7,14 @@ const port = process.env.PORT || 3000
 const user_router = require('./routers/users')
 const task_router = require('./routers/task')
 
+const multer = require('multer')
+const upload = multer({
+    dest: 'images'
+})
+
+app.post('/upload', upload.single('upload'), (req, res) => {
+    res.send()
+})
 // app.use((req, res, next) => {
 //     res.status(503).send('Server are maintenance!')
 // })
