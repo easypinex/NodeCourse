665477b45ecbd3b69ffb6c0010a428cc07996587
Andy <easypinex@gmail.com>
2023-07-18 16:01:53 +0800

    16-143 - course - Using SuperTest - Login

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -1,10 +1,27 @@
 const requests = require('supertest')
 const app = require('../src/app')
+const User = require('../src/models/user')
 
+const userOne = {
+    name: 'Mike',
+    email: 'mike@example.com',
+    password: 'wtfallday!!'
+}
+beforeEach(async () => {
+    await User.deleteMany()
+    await new User(userOne).save()
+})
 test('Should signup a new user', async () => {
     await requests(app).post('/users').send({
         name: 'Perry',
-        email: 'easypinex@gmail.com',
+        email: 'perry@test.com',
         password: 'mygoodpasshere'
     }).expect(201)
+})
+
+test('Sholud login existing user', async () => {
+    await requests(app).post('/users/login').send({
+        email: userOne.email,
+        password: userOne.password
+    }).expect(200)
 })
