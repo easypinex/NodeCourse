9e8ba88dcfb5e2bc2fc2db97e8fdb73a1234e8c3
Andy <easypinex@gmail.com>
2023-07-19 10:12:24 +0800

    16-144 - course - Testing with Authentication

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -1,16 +1,23 @@
 const requests = require('supertest')
 const app = require('../src/app')
 const User = require('../src/models/user')
+const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
 
+const userOneId = new mongoose.Types.ObjectId()
 const userOne = {
+    _id: userOneId,
     name: 'Mike',
     email: 'mike@example.com',
-    password: 'wtfallday!!'
+    password: 'wtfallday!!',
+    tokens: [{ token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET) }]
 }
+
 beforeEach(async () => {
     await User.deleteMany()
     await new User(userOne).save()
 })
+
 test('Should signup a new user', async () => {
     await requests(app).post('/users').send({
         name: 'Perry',
@@ -31,4 +38,19 @@ test('Sholud not login nonexisting user', async () => {
         email: 'nonexistuser@gmail.com',
         password: 'somepasswor'
     }).expect(400)
+})
+
+test('Sholud get profile for user', async () => {
+    await requests(app)
+            .get('/users/me')
+            .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+            .send()
+            .expect(200)
+})
+
+test('Sholud not get profile for unauthenticated user', async() => {
+    await requests(app)
+            .get('/users/me')
+            .send()
+            .expect(401)
 })
