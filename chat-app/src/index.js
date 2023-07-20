0bb952877b75849738dd8ec37b29e6e5dcbc1740
Andy <easypinex@gmail.com>
2023-07-20 13:34:34 +0800

    17-154- course - Setup SocketIO

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,12 +1,21 @@
 const express = require('express')
-const path =  require('path')
+const http = require('http')
+const path = require('path')
+const socketio = require('socket.io')
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
 const port = process.env.PORT || 3000
 const staticDir = path.join(__dirname, '..', 'public')
 
 app.use(express.static(staticDir))
 
-app.listen(port, () => {
+io.on('connection', () => {
+    console.log('New Socket Connection.')
+})
+
+server.listen(port, () => {
     console.log(`Server is up on port ${port}.`)
 })
