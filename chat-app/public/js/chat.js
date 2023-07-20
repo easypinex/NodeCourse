d17bcd757a6a6e678874bae49ee2e0553ae58496
Andy <easypinex@gmail.com>
2023-07-20 16:41:45 +0800

    17-158- challenge - Share Location

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -21,6 +21,9 @@ document.querySelector('#send_location').addEventListener('click', () => {
     if (!navigator.geolocation)
         return alert('Geolocation is not supported by your browser.')
     navigator.geolocation.getCurrentPosition((position) => {
-        console.log(position)
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        })
     })
 })
