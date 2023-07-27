da058f077fe77de23ec44ca2aab7bf5e604f213e
Andy <easypinex@gmail.com>
2023-07-27 16:58:18 +0800

    17-161 - course - Using mustache Library to rendering dynamic html

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -10,17 +10,23 @@ const socket = io()
 const $messageForm = document.querySelector('#message-form')
 const $messageFormInput = $messageForm.querySelector('input')
 const $messageFormButton = $messageForm.querySelector('button')
+const $messages = document.querySelector('#messages')
+const messageTemplate = document.querySelector('#message-template').innerHTML
 
 socket.on('message', (message) => {
     console.log(message)
+    const html = Mustache.render(messageTemplate, {
+        message
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
 })
 
 $messageForm.addEventListener('submit', (e) => {
     e.preventDefault()
+    const value = e.target.elements.message.value
     $messageFormButton.setAttribute('disabled', 'disabled')
     $messageFormInput.value = ''
     $messageFormInput.focus()
-    const value = e.target.elements.message.value
     socket.emit('sendMessage', value, (error) => {
         $messageFormButton.removeAttribute('disabled')
         if (error) {
